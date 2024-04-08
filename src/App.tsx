8aa187100b36a0a40f3f15c7bdcc3924d499da28
Ensil <lasags@naver.com>
2024-04-08 19:51:56 +0900

    UPDATE 상품 목업 데이터

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ const PRODUCTS = [
     { category: 'Fruits', price: '$1', stocked: true, name: 'Apple' },
     { category: 'Fruits', price: '$1', stocked: true, name: 'Dragonfruit' },
     { category: 'Fruits', price: '$2', stocked: false, name: 'Passionfruit' },
-    { category: 'Vegetables', price: '$2', stocked: true, name: 'Spinach' },
     { category: 'Vegetables', price: '$4', stocked: false, name: 'Pumpkin' },
     { category: 'Vegetables', price: '$1', stocked: true, name: 'Peas' },
+    { category: 'Vegetables', price: '$3', stocked: true, name: 'Carrot' },
+    { category: 'Vegetables', price: '$4', stocked: false, name: 'Garlic' },
+    { category: 'Vegetables', price: '$5', stocked: true, name: 'Cabbage' },
+    
 ];
 
 function App() {
